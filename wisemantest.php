ca42e1b4e4c67b727458b0e12c9d3a257af17034
Glenn Harris <glenn.harris@gmail.com>
2014-04-24 13:52:09 +1000

    upload files

---
+++ wisemantest.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>		
+<meta charset="utf-8" />
+<!--[if IE]><![endif]-->
+<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+<title>WISEMAN FINANCIAL SERVICES</title>
+<!-- FACEBOOK SHARE TAGS -->
+<meta property="og:title" content="Wiseman Financial Services" /> 
+<meta property="og:description" content="Wiseman Financial Services" /> 
+<meta property="og:image" content="http://www.wisemanfs.com.au/./uploads/fb-share-img.jpg" />
+
+<meta name="keywords" content="Financial Planning, Centrelink, Aged Care, Estate Planning, Superannuation, Insurance, Investments, financial advice" />
+<meta name="description" content="Wiseman Financial Services" />
+<meta name="author" content="Wisdom Advertising and Design (www.wisdom.com.au)" />
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
+<link rel="canonical" href="http://www.wisemanfs.com.au/" />
+<link rel="shortcut icon" href="http://www.wisemanfs.com.au/favicon.ico" />
+<link rel="stylesheet" media="screen,print" href="http://www.wisemanfs.com.au/includes/template/css/style.css?v=1" />
+<link rel="stylesheet" media="screen,print" href="wfsform.css" />
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
+<script type="text/javascript" src="address.js"></script>
+
+<!--[if lt IE 7]><link rel="stylesheet" media="screen,print" href="http://www.wisemanfs.com.au/includes/template/css/ie7.css?v=1" /><![endif]-->
+
+<!--[if lt IE 9]><script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script><![endif]-->	</head>
+	<!--[if lt IE 7]><body class="ie6"><![endif]-->
+	<!--[if IE 7]><body class="ie7"><![endif]-->
+	<!--[if IE 8]><body class="ie8"><![endif]-->
+	<!--[if IE 9]><body class="ie9"><![endif]-->
+	<!--[if gt IE 9]><body><![endif]-->
+	<!--[if !IE]><!--><body onload="initialize()"><!--<![endif]-->
+<div id="wrapper">
+<header>
+	<div id="page-logos">
+		<a href="http://www.wisemanfs.com.au/home" class="left"><img src="http://www.wisemanfs.com.au/includes/template/img/wiseman-logo.jpg" alt="" /></a>
+		<a href="http://www.wisemanfs.com.au/home" class="right"><img src="http://www.wisemanfs.com.au/includes/template/img/financial-services.jpg" alt="" /></a>
+	</div>
+	
+	<div id="page-banner">
+		<div id="banners">
+		<img src="http://www.wisemanfs.com.au/resize.php/page-banner-1.jpg?width=626&amp;height=201&amp;image=/./includes/template/img/banners/page-banner-1.jpg" />
+		<img src="http://www.wisemanfs.com.au/resize.php/page-banner-2.jpg?width=626&amp;height=201&amp;image=/./includes/template/img/banners/page-banner-2.jpg" class="banner-image" />
+		<img src="http://www.wisemanfs.com.au/resize.php/page-banner-3.jpg?width=626&amp;height=201&amp;image=/./includes/template/img/banners/page-banner-3.jpg" class="banner-image" />
+		<img src="http://www.wisemanfs.com.au/resize.php/page-banner-4.jpg?width=626&amp;height=201&amp;image=/./includes/template/img/banners/page-banner-4.jpg" class="banner-image" />
+	</div>
+		
+	<div id="header-icons">
+		<a href="http://www.facebook.com/sharer.php?u=http%3A%2F%2Fwww.wisemanfs.com.au%2Fhome%2F11&src=sp" target="_blank" id="speech-share-link"><img src="http://www.wisemanfs.com.au/includes/template/img/share-icon.png" alt="" /></a>
+			
+		<a href="http://www.facebook.com/pages/Wiseman-Financial-Services/183035351709937" target="_blank" id="speech-facebook-link"><img src="http://www.wisemanfs.com.au/includes/template/img/facebook-icon.png" alt="" /></a>
+			
+		<a href="http://www.wisemanfs.com.au/testimonials" id="speech-testimonials-link"><img src="http://www.wisemanfs.com.au/includes/template/img/testimonials-icon.png" alt="" /></a>
+	</div>
+	<div id="speech-share" class="speech-icons"></div>
+	<div id="speech-facebook" class="speech-icons"></div>
+	<div id="speech-testimonials" class="speech-icons"></div>
+</div>
+<nav>
+	<ul>
+		<li id="page_381" class="home">
+			<a href="http://www.wisemanfs.com.au/home" class="page home">home</a>
+		</li>
+		<li id="page_431" class="our-team">
+			<a href="http://www.wisemanfs.com.au/about-us" class="page about-us">About Us</a>
+		</li>
+		<li id="page_435" class="default">
+			<a href="http://www.wisemanfs.com.au/personal" class="page personal">Personal</a>
+		</li>
+		<li id="page_436" class="default">
+			<a href="http://www.wisemanfs.com.au/business" class="page business">Business</a>
+		</li>
+		<li id="page_433" class="contact">
+			<a href="http://www.wisemanfs.com.au/contact" class="page contact">Contact</a>
+		</li>
+	</ul>	
+</nav>	
+</header>			
+			
+<div id="page_content" class="editable"><h1>Online Advice</h1>
+<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis vehicula risus non rutrum. Nulla feugiat hendrerit odio. Morbi eget laoreet elit. Nulla mauris erat, iaculis eget velit in, viverra placerat risus. Morbi a volutpat elit. Ut pellentesque pellentesque ipsum, quis lacinia magna hendrerit at.</p>
+
+<div >
+	<label>First Name</label><input type="text" /><br />
+	<label>Last Name</label><input type="text" /><br />
+	<div id="locationField">
+		<label>Address</label>
+		<input id="autocomplete" onFocus="geolocate()" type="text" /><br />
+    </div>
+	<div style="display:none;">
+	<input class="field" id="street_number" disabled="true" />
+    <input class="field" id="route" disabled="true" />
+    <input class="field" id="locality" disabled="true" />
+	<input class="field" id="administrative_area_level_1" disabled="true" />
+    <input class="field" id="postal_code" disabled="true" />
+    <input class="field" id="country" disabled="true" />
+	</div>
+	<label>Email</label><input type="email" placeholder="E.g. name@company.com" /><br />
+	<label>Phone</label><input type="tel" placeholder="Home, work or mobile"/></br>
+	<label>Date of Birth</label><input type="date" /><br />
+	<label>Marital Status</label>
+	<select name="mstatus">
+		<option value="" selected disabled style="display:none">Select from the list</option>
+		<option value="0">Single</option>
+		<option value="1">Married</option>
+		<option value="2">Divorced</option>
+		<option value="3">Other</option>
+	</select>
+	
+</div>
+</div>
+
+<footer>		
+	<div id="footer-meritum"><a href="http://www.meritum.com.au/" target="_blank">Authorised Representatives of Meritum Financial Group Pty Ltd</a><br /><a href="http://www.wisemanfs.com.au/">(ACN 106 888 215) (AFSL No:245569)</a></div>
+	<div id="footer-wisdom"><a href="http://www.wisdom.com.au" target="_blank">Site by Wisdom</a></div>
+</footer>			
+</div>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+		<!--
+		<script src="http://www.wisemanfs.com.au/includes/template/js/dd_belatedpng.js" type="text/javascript"></script>
+		<script src="http://www.wisemanfs.com.au/includes/template/js/modernizr-1.5.min.js" type="text/javascript"></script>
+		-->
+		
+<script src="http://www.wisemanfs.com.au/includes/template/js/jquery.fade.js" type="text/javascript"></script>
+<script src="http://www.wisemanfs.com.au/includes/template/js/script.js"></script>
+<script type="text/javascript">
+			$(document).ready(function() {			
+				$('header nav li').removeClass('active');
+				$('header nav li a.home').parent('li').addClass('active');
+				$('header nav li a.home').parent('li').parent('ul').parent('li').addClass('active');		
+			});
+</script>
+		
+<script type="text/javascript">var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-5139206-40']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+</body>
+</html>
